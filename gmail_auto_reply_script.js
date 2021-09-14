fb83101238edaece10da73f3b4c287713642b63d
Arie van der Poel <arie@mediasuite.co.nz>
2021-09-14 18:24:56 +1200

    Tweak for streamside orgaics

--- gmail_auto_reply_script.js
+++ gmail_auto_reply_script.js
@@ -1,11 +1,10 @@
 function autoReply() {
   const interval = 5 // if the script runs every 5 minutes change otherwise
-  const daysToSendMessage = [2,4] // 1=Mo, 2=Tu, 3=We, 4=Th, 5=Fr, 6=Sa, 0=Su
-  const message = "Hi, Thank you for contacting us. The office is now closed. All emails will be attended to when the office re-opens."
+  const message = "Thanks for emailing Streamside Organics.\n\nIf you are emailing about your veggie box delivery, please note that whilst we try to get all boxes delivered by a reasonable time some boxes may be delivered up until 9pm.\n\nIf you have not received your box by 9pm please let us know by email.\n\nWe will follow up first thing in the morning.\n\nThanks for supporting your local farmers.\n\nStreamside Organics"
   const date = new Date()
   const day = date.getDay()
-  // const hour = date.getHours()
-  if (daysToSendMessage.includes(day)) {
+  const hour = date.getHours()
+  if (day === 4 && hour >= 17 || day === 6) { // 1=Mo, 2=Tu, 3=We, 4=Th, 5=Fr, 6=Sa, 0=Su
     const timeFrom = Math.floor(date.valueOf()/1000) - 60 * interval
     const threads = GmailApp.search('is:inbox after:' + timeFrom)
     for (let i = 0; i < threads.length; i++) {
