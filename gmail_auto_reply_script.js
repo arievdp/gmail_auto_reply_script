fa67c99c2fd8bd1b0be6b1747bb57afcad3068d9
Arie van der Poel <arie@mediasuite.co.nz>
2021-09-14 17:26:13 +1200

    Formatting

--- gmail_auto_reply_script.js
+++ gmail_auto_reply_script.js
@@ -1,18 +1,19 @@
 function autoReply() {
-  const interval = 5; // if the script runs every 5 minutes; change otherwise
-  const daysToSendMessage = [2,4]; // 1=Mo, 2=Tu, 3=We, 4=Th, 5=Fr, 6=Sa, 0=Su
-  const message = "Hi, Thank you for contacting us. The office is now closed. All emails will be attended to when the office re-opens.";
-  const date = new Date();
-  const day = date.getDay();
-  // const hour = date.getHours();
+  const interval = 5 // if the script runs every 5 minutes change otherwise
+  const daysToSendMessage = [2,4] // 1=Mo, 2=Tu, 3=We, 4=Th, 5=Fr, 6=Sa, 0=Su
+  const message = "Hi, Thank you for contacting us. The office is now closed. All emails will be attended to when the office re-opens."
+  const date = new Date()
+  const day = date.getDay()
+  // const hour = date.getHours()
   if (daysToSendMessage.indexOf(day)) {
-    const timeFrom = Math.floor(date.valueOf()/1000) - 60 * interval;
-    const threads = GmailApp.search('is:inbox after:' + timeFrom);
+    const timeFrom = Math.floor(date.valueOf()/1000) - 60 * interval
+    const threads = GmailApp.search('is:inbox after:' + timeFrom)
     for (let i = 0; i < threads.length; i++) {
-      threads[i].reply(message);
+      threads[i].reply(message)
     }
   }
-  }
-  function doGet() {
-   return ContentService.createTextOutput("");
-  }
+}
+
+function doGet() {
+  return ContentService.createTextOutput("");
+}
